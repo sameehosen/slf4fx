86b55f3f83cd37578bc3d68450f9b9dd801c1200
dmotylev@room13.org <dmotylev@room13.org@65a6454c-3d55-0410-85c1-6dc2c109ab04>
2008-09-05 16:58:24 +0000

    import client modules

--- trunk/slf4fx-server/src/main/java/org/room13/slf4fx/SLF4FxServer.java
+++ trunk/slf4fx-server/src/main/java/org/room13/slf4fx/SLF4FxServer.java
@@ -16,7 +16,7 @@ public class SLF4FxServer {
         final MessageSource resources = new ClassPathXmlApplicationContext("/META-INF/slf4fx-context.xml");
         final String version = resources.getMessage("slf4fx.version", null, null);
         final String buildNumber = resources.getMessage("slf4fx.buildNumber", null, null);
-        _log.info("slf4fx ({}.{}) server started in {}ms", new Object[]{
+        _log.info("slf4fx ({} build {}) server started in {}ms", new Object[]{
                 version, buildNumber, System.currentTimeMillis() - startTime});
     }
 }
