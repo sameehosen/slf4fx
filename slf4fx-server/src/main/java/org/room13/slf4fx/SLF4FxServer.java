e944187353f6c6a521bcddbd804b1901cee909cd
dmotylev <dmotylev@room13.org>
2008-09-09 20:38:11 +0000

    cosmetic changes

--- slf4fx-server/src/main/java/org/room13/slf4fx/SLF4FxServer.java
+++ slf4fx-server/src/main/java/org/room13/slf4fx/SLF4FxServer.java
@@ -30,6 +30,6 @@ public class SLF4FxServer {
         final long startTime = System.currentTimeMillis();
         final MessageSource resources = new ClassPathXmlApplicationContext("/META-INF/slf4fx-context.xml");
         final String version = resources.getMessage("slf4fx.version", null, null);
-        _log.info("slf4fx ({}) server started in {}ms", new Object[]{version, System.currentTimeMillis() - startTime});
+        _log.info("slf4fx (version {}) server started in {}ms", new Object[]{version, System.currentTimeMillis() - startTime});
     }
 }
