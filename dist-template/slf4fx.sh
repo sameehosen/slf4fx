0b81377c37c5d4f00eeadcbfd12190a552e3ad80
dmotylev <dmotylev@room13.org>
2008-10-24 10:15:42 +0000

    modify slf4fx.sh to support linking

--- dist-template/slf4fx.sh
+++ dist-template/slf4fx.sh
@@ -1,2 +1,21 @@
 #!/bin/sh
-java -jar slf4fx-server.jar -p crossdomain.xml -r 100000
+CURRENT_DIR=`pwd`
+
+LINK_DIR=`dirname "$0"`
+cd "${LINK_DIR}"
+PRG_NAME=`basename "$0"`
+PRG="${PRG_NAME}"
+
+while [ -h "$PRG" ] ; do
+  ls=`ls -ld "$PRG"`
+  LINK=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$LINK" : '.*/.*' > /dev/null; then
+    PRG="${LINK}"
+  else
+    PRG="`dirname ${PRG}`/${LINK}"
+  fi
+done
+
+SLF4FX_HOME=`dirname "$PRG"`
+cd ${CURRENT_DIR}
+java -jar "${SLF4FX_HOME}/slf4fx-server.jar" -p "${SLF4FX_HOME}/crossdomain.xml" 
